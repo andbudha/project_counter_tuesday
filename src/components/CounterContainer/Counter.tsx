e5a5ed1f68b792bb5f8b7c5221dfcb99dc52b109
andbudha <andreibartov@gmail.com>
2023-02-10 11:33:17 +0100

    final styles added

--- src/components/CounterContainer/Counter.tsx
+++ src/components/CounterContainer/Counter.tsx
@@ -5,11 +5,23 @@ export const Counter = () => {
     return (
         <div className={styles.main_container}>
             <div className={styles.counter_container}>
-
+                <div className={styles.set_display}>
+                    <div className={styles.val_container}>
+                        <h2>max-value:</h2>
+                        <input type="number"/>
+                    </div>
+                    <div className={styles.val_container}>
+                        <h2>start-value:</h2>
+                        <input type="number"/>
+                    </div>
+                </div>
+                <div className={styles.set_btn_container}>
+                    <Button/>
+                </div>
             </div>
             <div className={styles.counter_container}>
                 <div className={styles.incr_display}>0</div>
-                <div className={styles.btn_container}>
+                <div className={styles.incr_btn_container}>
                     <Button/>
                     <Button/>
                 </div>
