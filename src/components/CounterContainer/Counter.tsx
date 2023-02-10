73c2f23b95f1e488c3cb1447dc0aa97703315875
andbudha <andreibartov@gmail.com>
2023-02-10 10:45:01 +0100

    components created/encapsulated

---
+++ src/components/CounterContainer/Counter.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import  styles from './Counter.module.css'
+export const Counter = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
---
+++ src/components/VersatileButton/Button.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './Button.module.css'
+export const Button = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
