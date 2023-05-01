df65e95d5dbd0dc29952d0e4fd6b5b6063795e42
andbudha <andreibartov@gmail.com>
2023-05-01 14:32:14 +0200

    button-com updated/startValReducer added

---
+++ src/reducers/startvaluereducer.ts
@@ -0,0 +1,13 @@
+
+export type StartValStateType = number;
+
+type StartValAcType = any;
+export const startValReducer = (state: StartValStateType, action: StartValAcType) => {
+    switch (action.type) {
+        case 'XXX': {
+            return state;
+        } default: {
+            return state;
+        }
+    }
+}
