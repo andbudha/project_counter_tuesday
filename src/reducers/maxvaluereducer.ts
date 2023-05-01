2631d702ec6361d8e16fb9d8ca795f7299553d7b
andbudha <andreibartov@gmail.com>
2023-05-01 14:36:44 +0200

    maxValReducer added

---
+++ src/reducers/maxvaluereducer.ts
@@ -0,0 +1,13 @@
+
+export type MaxValStateType = number;
+
+type MaxValAcType = any;
+export const maxValReducer = (state: MaxValStateType, action: MaxValAcType) => {
+    switch (action.type) {
+        case 'XXX': {
+            return state;
+        } default : {
+            return  state;
+        }
+    }
+}
