8f374f5a21c7f210d545ffaebe4fa84a7e3bf8bc
andbudha <andreibartov@gmail.com>
2023-05-01 14:58:26 +0200

    maxvaluereducer.ts.ts updated

--- src/reducers/maxvaluereducer.ts
+++ src/reducers/maxvaluereducer.ts
@@ -1,13 +1,24 @@
 
 export type MaxValStateType = number;
 
-type MaxValAcType = any;
-export const maxValReducer = (state: MaxValStateType, action: MaxValAcType) => {
+const InitialState: MaxValStateType = 0;
+
+type MaxValAcType = maxValSettingACType;
+export const maxValReducer = (state: MaxValStateType = InitialState, action: MaxValAcType): MaxValStateType => {
     switch (action.type) {
-        case 'XXX': {
-            return state;
-        } default : {
+        case "SET-MAX-VALUE": {
+            return action.payload.value;
+        }
+        default : {
             return  state;
         }
     }
+}
+
+type maxValSettingACType = ReturnType<typeof maxValSettingAC>
+export const maxValSettingAC = (value: number) => {
+    return {
+        type: 'SET-MAX-VALUE',
+        payload: { value }
+    } as const
 }
