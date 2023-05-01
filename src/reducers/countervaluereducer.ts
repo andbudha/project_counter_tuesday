ceaa2c3b8b796381785cb3e8559ea96708beefb8
andbudha <andreibartov@gmail.com>
2023-05-01 14:51:43 +0200

    countervaluereducer.ts updated

--- src/reducers/countervaluereducer.ts
+++ src/reducers/countervaluereducer.ts
@@ -4,7 +4,7 @@ export type CounterValStateType = number;
 const InitialState: CounterValStateType = 0;
 
 type CounterValAcType = counterValSettingACType;
-export const counterValReducer = (state: CounterValStateType, action: CounterValAcType): CounterValStateType => {
+export const counterValReducer = (state: CounterValStateType = InitialState, action: CounterValAcType): CounterValStateType => {
     switch (action.type) {
         case "SET-COUNTER-VALUE": {
             return action.payload.value + 1;
